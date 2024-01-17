95f28894a9cb2e8d4d50bbb443f3bd1dd3472d3b
Lee Youngeung <20eung@gmail.com>
2024-01-17 09:40:51 +0900

    Update run.py

--- run.py
+++ run.py
@@ -5,13 +5,20 @@ from naver import find as f
 base_url = "https://www.clien.net/service/board/jirum"
 
 if __name__ == "__main__":
-    s = s.session("##ID##", "##PASSWORD##")
+    driver = s.session("20eung", "ZFHUJEUXFLBG")
+#   driver = s("20eung", "ZFHUJEUXFLBG")
     campaign_links = f.find(base_url)
+
     if campaign_links == []:
         print("모든 링크를 방문했습니다.")
+
     for link in campaign_links:
-        response = s.get(link)
-        print(response.text)  # for debugging
-        response.raise_for_status()
+        driver.get(link)
+        page_source = driver.page_source
+
+        print(page_source)  # for debugging
+
         time.sleep(5)
         print("캠페인 URL : " + link)
+
+    driver.quit()
