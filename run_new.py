a2c250a58ff43f337cc769a21512b1a53243fc2a
DoYoub Lee <bluesid@naver.com>
2024-11-06 05:53:30 +0900

    modify

--- run_new.py
+++ run_new.py
@@ -234,11 +234,11 @@ if __name__ == "__main__":
             args.credential_file is None):
         id = os.getenv("USERNAME")
         pw = os.getenv("PASSWORD")
-        cd_env = os.getenv("CREDENTIALENV")
+        cd_env = os.getenv("CREDENTIALENV", None)
         if(pw is None and pw is None and cd_env is None):
             print('not setting USERNAME / PASSWORD')
             exit()
-        if cd_env is None:
+        if cd_env is None and cd_env == "":
             cd_obj = [{"id": id, "pw": pw}]
         else:
             cd_obj = json.loads(cd_env)
